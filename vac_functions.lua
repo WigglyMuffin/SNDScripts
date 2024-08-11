4bca888b9884d0d0189ebc74c75319600911b16d
CurlyWorm <curlyworm78@gmail.com>
2024-08-12 01:39:40 +0200

    fixes

--- vac_functions.lua
+++ vac_functions.lua
@@ -884,3 +884,122 @@ end
     -- --return math.sqrt((x2 - x1)^2 + (y2 - y1)^2 + (z2 - z1)^2)
     -- return zoobz
 -- end
+
+-- Usage: PathToChar("First Last")
+-- Finds specified character and paths to it
+function PathToChar(path_char_name)
+    Movement(GetObjectRawXPos(path_char_name), GetObjectRawYPos(path_char_name), GetObjectRawZPos(path_char_name))
+end
+
+-- Finds where your estate entrance is and paths to it
+-- I think this will break though since there will be multiple entrances around a housing area
+-- So maybe needs nearest logic
+function PathToEstateEntrance()
+    Movement(GetObjectRawXPos("Entrance"), GetObjectRawYPos("Entrance"), GetObjectRawZPos("Entrance"))
+end
+
+-- Paths to Limsa bell
+function PathToLimsaBell()
+    if ZoneCheck(129, "Limsa", "tp") then
+        -- stuff could go here
+    else
+        Movement(-123.72, 18.00, 20.55)
+    end
+end
+
+-- Usage: WaitForGilIncrease(1)
+-- Obtains current gil and waits for specified gil to be traded
+-- Acts as the trigger before moving on
+function WaitForGilIncrease(gil_increase_amount)
+    -- Gil variable store before trade
+    local previous_gil = GetGil()
+    
+    while true do
+        Sleep(1.0) -- Wait for 1 second between checks
+        
+        local current_gil = GetGil()
+        if current_gil > previous_gil and (current_gil - previous_gil) == gil_increase_amount then
+            Echo(gil_increase_amount .. "Gil successfully traded")
+            break -- Exit the loop when the gil increase is detected
+        end
+        
+        previous_gil = current_gil -- Update gil amount for the next check
+    end
+end
+
+-- Usage: PartyInvite("First Last")
+-- Will target and invite player to a party, and retrying if the invite timeout happens
+-- Probably think of a way to invite without needing a placeholder aka <t>
+function PartyInvite(player_invite_name)
+    local invite_timeout = 305 -- 300 Seconds is the invite timeout, adding 5 seconds for good measure
+    local start_time = os.time() -- Stores the invite time
+    
+    while not IsInParty() do
+        repeat
+            Sleep(0.1)
+        until IsPlayerAvailable() and not IsPlayerCasting() and not GetCharacterCondition(26)
+        
+        Target(player_invite_name)
+        yield("/invite")
+        
+        -- Wait for the target player to accept the invite or the timeout to expire
+        while not IsInParty() do
+            Sleep(0.1)
+            
+            -- Check if the invite has expired
+            if os.time() - start_time >= invite_timeout then
+                Echo("Invite expired. Reinviting " .. player_invite_name)
+                start_time = os.time() -- Reset the start time for the new invite
+                break -- Break the loop to resend the invite
+            end
+        end
+    end
+    -- stuff could go here
+end
+
+-- Usage: PartyDisband()
+-- Will check if player is in party and disband party
+function PartyDisband()
+    if IsInParty() then
+        repeat
+            Sleep(0.1)
+        until IsPlayerAvailable() and not IsPlayerCasting() and not GetCharacterCondition(26)
+        
+        yield("/partycmd disband")
+        
+        repeat
+            Sleep(0.1)
+            yield("/pcall SelectYesno true 0")
+        until not IsAddonVisible("SelectYesno")
+    end
+end
+
+-- Usage: EstateTeleport("First Last", 0)
+-- Options: 0 = Free Company, 1 = Personal, 2 = Apartment
+-- Opens estate list of added friend and teleports to specified location
+-- ZoneTransitions() not required
+function EstateTeleport(estate_char_name, estate_type)
+    repeat
+        Sleep(0.1)
+    until IsPlayerAvailable() and not IsPlayerCasting() and not GetCharacterCondition(26)
+    
+    yield("/estatelist " .. estate_char_name)
+    
+    repeat
+        Sleep(0.1)
+        yield("/pcall TeleportHousingFriend true " .. estate_type)
+    until not IsAddonVisible("TeleportHousingFriend")
+    
+    ZoneTransitions()
+end
+
+-- Usage: RelogCharacter("First Last@Server")
+-- Relogs specified character, should be followed with a LoginCheck()
+-- Requires @Server else it will not work
+function RelogCharacter(relog_char_name)
+    repeat
+        Sleep(0.1)
+    until IsPlayerAvailable() and not IsPlayerCasting() and not GetCharacterCondition(26)
+    
+    yield("/ays relog " .. relog_char_name)
+end
