b8b9ed582751aa17841ef9e5e4053fa7421d8c79
Friendly <78714430+barnicskolaci@users.noreply.github.com>
2025-06-12 00:10:31 +0100

    Update AutoHunt - Helper.lua

--- Scripts/AutoHunt/AutoHunt - Helper.lua
+++ Scripts/AutoHunt/AutoHunt - Helper.lua
@@ -57,7 +57,43 @@ function GetDutyTimer()
 end
 
 repeat
-    if GetDutyInfoText(2) == "Clear the feasting hall: 0/1" and GetTargetName() == "All-seeing Eye" then
+    while not IsPlayerAvailable() do
+        Sleep(1.0534)
+    end
+                local Terminal_List = {
+                [1] = {
+                    ["Name"] = "III",
+                    ["Coords"] = "124 -13.8 123.4"
+                },
+                [2] = {
+                    ["Name"] = "IV",
+                    ["Coords"] = "140.75 -12 113.2"
+                },
+                [3] = {
+                    ["Name"] = "IX",
+                    ["Coords"] = "-14.9 -21 -143.97"
+                },
+                [4] = {
+                    ["Name"] = "VIII",
+                    ["Coords"] = "-16.32 -17.3 -177.65"
+                }
+            }
+            for i=1, #Terminal_List do
+                if GetToastNodeText(2, 3) == "Magitek terminal "..Terminal_List[i]["Name"].." begins counting down." then
+                    local counter = 0
+                    repeat
+                        ClearTarget()
+                        yield("/vnav moveto "..Terminal_List[i]["Coords"])
+                        Sleep(0.22)
+                        counter = counter+1
+                    until GetToastNodeText(2, 3) == "Magitek terminal "..Terminal_List[i]["Name"].."'s countdown completes." or counter > 200
+                end
+            end
+    if duty_timer and duty_timer < 4950 and GetDutyInfoText(2) == "Clear the feasting hall: 1/1" and GetToastNodeText(2, 3) == "Magitek terminal "..Terminal_List[3]["Name"].."'s countdown completes." then
+        yield("/vnav moveto "..Terminal_List[4]["Coords"])
+    elseif duty_timer and duty_timer < 5010 and GetDutyInfoText(2) == "Clear the feasting hall: 1/1" then
+        yield("/vnav moveto "..Terminal_List[3]["Coords"])
+    elseif GetDutyInfoText(2) == "Clear the feasting hall: 0/1" and GetTargetName() == "All-seeing Eye" then
         local Crystal_Coords = {
             [1] = {
                 ["Coords"] = "17.4 -13.8 86.7"
@@ -73,19 +109,23 @@ repeat
             }
         }
         for i=1, #Crystal_Coords do  --just taking the boss to the crystals
-            for i=1, 50 do
+            repeat
                 yield("/vnav moveto "..Crystal_Coords[i]["Coords"])
-                Sleep(2.00819)
-                repeat
-                    Sleep(0.20824)
-                until GetToastNodeText(2, 3) == "The power of the crystal begins to dim." or not DoesObjectExist("All-seeing Eye")
-            end
+                Sleep(1.0824)
+                if HasStatus("Crystal Veil") then
+                    break
+                end
+            until not HasStatus("Crystal Veil") or not DoesObjectExist("All-seeing Eye")
         end
+    elseif duty_timer and duty_timer < 5190 and GetDutyInfoText(1) == "Open the grand hall gate: 0/1" and GetToastNodeText(2, 3) == "Magitek terminal "..Terminal_List[1]["Name"].."'s countdown completes." then
+        yield("/vnav moveto "..Terminal_List[2]["Coords"])
+    elseif duty_timer and duty_timer < 5250 and GetDutyInfoText(1) == "Open the grand hall gate: 0/1" then
+        yield("/vnav moveto "..Terminal_List[1]["Coords"])
     end
     local duty_timer = GetDutyTimer()
     if duty_timer and duty_timer < 4200 then
         yield("/ad stop")
         LeaveDuty()
     end
-    Sleep(5)
+    Sleep(2)
 until forever
