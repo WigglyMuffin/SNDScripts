99642e581e1ae18d6f009f3f6d210fe04c2eaa73
Friendly <78714430+barnicskolaci@users.noreply.github.com>
2025-06-18 18:17:49 +0100

    Update vac_functions.lua
    2.0.0 and 2.0.1

--- vac_functions.lua
+++ vac_functions.lua
@@ -8,8 +8,10 @@
 
 ####################
 ##    Version     ##
-##     1.1.3      ##
+##     2.0.0      ##
 ####################
+2.0.0: SND update. All of these and the old SND commands (pasted near the end, thanks Faye!) must be rewritten/wrapped. Contributions welcome!
+2.0.1: (Nonu) IsPlayerCasting(), IsPlayerAvailable() and GetCharacterCondition()
 
 -> 1.0.0: Initial release
 -> 1.0.1: Updated UseFCAction()
@@ -5730,6 +5732,962 @@ function LeaveDuty()
         Sleep(0.1)
     end
 end
+
+-- Old SND commands (plus other stuff that may need to be removed
+
+---@diagnostic disable: missing-return
+
+--- All functions in this file are provided by Something Need Doing
+--- at run time, this file exists to assist with code completion by
+--- allowing the language server access to the annotations.
+---
+--- This file is a work in progress and still needs heavy annotation.
+
+function ADContentHasPath() end
+
+function ADGetConfig() end
+
+function ADIsLooping() end
+
+function ADIsNavigating() end
+
+function ADIsStopped() end
+
+function ADListConfig() end
+
+function ADRun() end
+
+function ADSetConfig() end
+
+function ADStart() end
+
+function ADStop() end
+
+function ARAbortAllTasks() end
+
+function ARAnyWaitingToBeProcessed() end
+
+function ARAreAnyRetainersAvailableForCurrentChara() end
+
+function ARCanAutoLogin() end
+
+function ARDisableAllFunctions() end
+
+function ARDiscardGetItemsToDiscard() end
+
+function AREnableMultiMode() end
+
+function AREnqueueHET() end
+
+function AREnqueueInitiation() end
+
+function ARFinishCharacterPostProcess() end
+
+function ARGetCharacterCIDs() end
+
+function ARGetCharacterData() end
+
+function ARGetClosestRetainerVentureSecondsRemaining() end
+
+function ARGetEnabledRetainers() end
+
+function ARGetGCInfo() end
+
+function ARGetInventoryFreeSlotCount() end
+
+function ARGetMultiModeEnabled() end
+
+function ARGetOptionRetainerSense() end
+
+function ARGetOptionRetainerSenseThreshold() end
+
+function ARGetRegisteredCharacters() end
+
+function ARGetRegisteredEnabledCharacters() end
+
+function ARGetRegisteredEnabledRetainers() end
+
+function ARGetRegisteredRetainers() end
+
+function ARIsBusy() end
+
+function ARRelog() end
+
+function ARRetainersWaitingToBeProcessed() end
+
+function ARSetMultiModeEnabled() end
+
+function ARSetOptionRetainerSense() end
+
+function ARSetOptionRetainerSenseThreshold() end
+
+function ARSetSuppressed() end
+
+function ARSubsWaitingToBeProcessed() end
+
+function ATAddItemToCraftList() end
+
+function ATAddNewCraftList() end
+
+function ATCurrentCharacter() end
+
+function ATDisableBackgroundFilter() end
+
+function ATDisableCraftList() end
+
+function ATDisableUiFilter() end
+
+function ATEnableBackgroundFilter() end
+
+function ATEnableCraftList() end
+
+function ATEnableUiFilter() end
+
+function ATGetCharacterItems() end
+
+function ATGetCharacterItemsByType() end
+
+function ATGetCharactersOwnedByActive() end
+
+function ATGetCraftItems() end
+
+function ATGetCraftLists() end
+
+function ATGetFilterItems() end
+
+function ATGetRetrievalItems() end
+
+function ATGetSearchFilters() end
+
+function ATInventoryCountByType() end
+
+function ATInventoryCountByTypes() end
+
+function ATIsInitialized() end
+
+function ATItemCount() end
+
+function ATItemCountHQ() end
+
+function ATItemCountOwned() end
+
+function ATRemoveItemFromCraftList() end
+
+function ATToggleBackgroundFilter() end
+
+function ATToggleCraftList() end
+
+function ATToggleUiFilter() end
+
+function ArtisanCraftItem() end
+
+function ArtisanGetEnduranceStatus() end
+
+function ArtisanGetStopRequest() end
+
+function ArtisanIsListPaused() end
+
+function ArtisanIsListRunning() end
+
+function ArtisanSetEnduranceStatus() end
+
+function ArtisanSetListPause() end
+
+function ArtisanSetStopRequest() end
+
+function BMAddTransientStrategy() end
+
+function BMAddTransientStrategyTargetEnemyOID() end
+
+function BMClearActive() end
+
+function BMCreate() end
+
+function BMDelete() end
+
+function BMGet() end
+
+function BMGetActive() end
+
+function BMGetForceDisabled() end
+
+function BMSetActive() end
+
+function BMSetForceDisabled() end
+
+function CanExtractMateria() end
+
+function ClearFocusTarget() end
+
+function ClearTarget() end
+
+function CrashTheGame() end
+
+function DeleteAllAutoHookAnonymousPresets() end
+
+function DeletedSelectedAutoHookPreset() end
+
+function DeliverooIsTurnInRunning() end
+
+function DistanceBetween() end
+
+function DoesObjectExist() end
+
+function DropboxGetItemQuantity() end
+
+function DropboxIsBusy() end
+
+function DropboxSetItemQuantity() end
+
+function DropboxStart() end
+
+function DropboxStop() end
+
+function Equals() end
+
+function ExecuteAction() end
+
+function ExecuteGeneralAction() end
+
+function FocusTargetHasStatus() end
+
+function GetAcceptedQuests() end
+
+function GetAccursedHoardRawX() end
+
+function GetAccursedHoardRawY() end
+
+function GetAccursedHoardRawZ() end
+
+function GetActionStackCount() end
+
+function GetActiveFates() end
+
+function GetActiveMacroName() end
+
+function GetActiveMiniMapGatheringMarker() end
+
+function GetActiveWeatherID() end
+
+function GetAddersGCRank() end
+
+function GetAetheryteList() end
+
+function GetAetheryteName() end
+
+function GetAetheryteRawPos() end
+
+function GetAetherytesInZone() end
+
+function GetBronzeChestLocations() end
+
+function GetBuddyTimeRemaining() end
+
+function GetCharacterCondition() end
+
+function GetCharacterName() end
+
+---@return Job
+function GetClassJobId() end
+
+function GetClicks() end
+
+function GetClipboard() end
+
+function GetCondition() end
+
+function GetContentTimeLeft() end
+
+function GetCp() end
+
+function GetCurrentBait() end
+
+---@return integer
+function GetCurrentEorzeaHour() end
+
+function GetCurrentEorzeaMinute() end
+
+function GetCurrentEorzeaSecond() end
+
+function GetCurrentEorzeaTimestamp() end
+
+function GetCurrentOceanFishingMission1Goal() end
+
+function GetCurrentOceanFishingMission1Name() end
+
+function GetCurrentOceanFishingMission1Progress() end
+
+function GetCurrentOceanFishingMission1Type() end
+
+function GetCurrentOceanFishingMission2Goal() end
+
+function GetCurrentOceanFishingMission2Name() end
+
+function GetCurrentOceanFishingMission2Progress() end
+
+function GetCurrentOceanFishingMission2Type() end
+
+function GetCurrentOceanFishingMission3Goal() end
+
+function GetCurrentOceanFishingMission3Name() end
+
+function GetCurrentOceanFishingMission3Progress() end
+
+function GetCurrentOceanFishingMission3Type() end
+
+function GetCurrentOceanFishingPoints() end
+
+function GetCurrentOceanFishingRoute() end
+
+function GetCurrentOceanFishingScore() end
+
+function GetCurrentOceanFishingStatus() end
+
+function GetCurrentOceanFishingTimeOfDay() end
+
+function GetCurrentOceanFishingTimeOffset() end
+
+function GetCurrentOceanFishingTotalScore() end
+
+function GetCurrentOceanFishingWeatherID() end
+
+function GetCurrentOceanFishingZone() end
+
+function GetCurrentOceanFishingZoneTimeLeft() end
+
+function GetCurrentWorld() end
+
+function GetDDPassageProgress() end
+
+function GetDiademAetherGaugeBarCount() end
+
+function GetDistanceToFocusTarget() end
+
+function GetDistanceToObject() end
+
+function GetDistanceToPartyMember() end
+
+function GetDistanceToPoint() end
+
+function GetDistanceToTarget() end
+
+function GetDurability() end
+
+function GetFCGrandCompany() end
+
+function GetFCOnlineMembers() end
+
+function GetFCRank() end
+
+function GetFCTotalMembers() end
+
+function GetFateChain() end
+
+function GetFateDuration() end
+
+function GetFateEventItem() end
+
+function GetFateHandInCount() end
+
+function GetFateIconId() end
+
+function GetFateIsBonus() end
+
+function GetFateLevel() end
+
+function GetFateLocationX() end
+
+function GetFateLocationY() end
+
+function GetFateLocationZ() end
+
+function GetFateMaxLevel() end
+
+function GetFateName() end
+
+function GetFateProgress() end
+
+function GetFateRadius() end
+
+function GetFateStartTimeEpoch() end
+
+function GetFateState() end
+
+function GetFlagXCoord() end
+
+function GetFlagYCoord() end
+
+function GetFlagZone() end
+
+function GetFlamesGCRank() end
+
+function GetFocusTargetActionID() end
+
+function GetFocusTargetFateID() end
+
+function GetFocusTargetHP() end
+
+function GetFocusTargetHPP() end
+
+function GetFocusTargetMaxHP() end
+
+function GetFocusTargetName() end
+
+function GetFocusTargetRawXPos() end
+
+function GetFocusTargetRawYPos() end
+
+function GetFocusTargetRawZPos() end
+
+function GetFocusTargetRotation() end
+
+function GetFreeSlotsInContainer() end
+
+function GetGil() end
+
+function GetGoldChestLocations() end
+
+function GetGp() end
+
+function GetHP() end
+
+function GetHashCode() end
+
+function GetHomeWorld() end
+
+function GetInventoryFreeSlotCount() end
+
+function GetItemCount() end
+
+function GetItemCountInContainer() end
+
+function GetItemCountInSlot() end
+
+function GetItemIdInSlot() end
+
+function GetItemIdsInContainer() end
+
+function GetItemName() end
+
+function GetJobExp() end
+
+function GetLevel() end
+
+function GetLimitBreakBarCount() end
+
+function GetLimitBreakBarValue() end
+
+function GetLimitBreakCurrentValue() end
+
+function GetMP() end
+
+function GetMaelstromGCRank() end
+
+function GetMaxCp() end
+
+function GetMaxDurability() end
+
+function GetMaxGp() end
+
+function GetMaxHP() end
+
+function GetMaxMP() end
+
+function GetMaxProgress() end
+
+function GetMaxQuality() end
+
+function GetMimicChestLocations() end
+
+function GetMonsterNoteRankInfo() end
+
+function GetNearbyObjectNames() end
+
+function GetNearestFate() end
+
+function GetNodeListCount() end
+
+function GetNodeText() end
+
+function GetObjectActionID() end
+
+function GetObjectDataID() end
+
+function GetObjectFateID() end
+
+function GetObjectHP() end
+
+function GetObjectHPP() end
+
+function GetObjectHitboxRadius() end
+
+function GetObjectHuntRank() end
+
+function GetObjectMaxHP() end
+
+function GetObjectRawXPos() end
+
+function GetObjectRawYPos() end
+
+function GetObjectRawZPos() end
+
+function GetObjectRotation() end
+
+function GetPartyLeadIndex() end
+
+function GetPartyMemberActionID() end
+
+function GetPartyMemberHP() end
+
+function GetPartyMemberHPP() end
+
+function GetPartyMemberMaxHP() end
+
+function GetPartyMemberName() end
+
+function GetPartyMemberRawXPos() end
+
+function GetPartyMemberRawYPos() end
+
+function GetPartyMemberRawZPos() end
+
+function GetPartyMemberRotation() end
+
+function GetPartyMemberWorldId() end
+
+function GetPartyMemberWorldName() end
+
+function GetPassageLocation() end
+
+function GetPenaltyRemainingInMinutes() end
+
+function GetPercentHQ() end
+
+function GetPlayerAccountId() end
+
+function GetPlayerContentId() end
+
+function GetPlayerGC() end
+
+function GetPlayerRawXPos() end
+
+function GetPlayerRawYPos() end
+
+function GetPlayerRawZPos() end
+
+function GetPluginVersion() end
+
+function GetProgress() end
+
+function GetProgressIncrease() end
+
+function GetQuality() end
+
+function GetQualityIncrease() end
+
+function GetQuestAlliedSociety() end
+
+function GetQuestIDByName() end
+
+function GetQuestSequence() end
+
+function GetRealRecastTime() end
+
+function GetRealRecastTimeElapsed() end
+
+function GetRealSpellCooldown() end
+
+function GetRecastTime() end
+
+function GetRecastTimeElapsed() end
+
+function GetRequestedAchievementProgress() end
+
+function GetSNDProperty() end
+
+function GetSelectIconStringText() end
+
+function GetSelectStringText() end
+
+function GetShieldPercentage() end
+
+function GetSilverChestLocations() end
+
+function GetSpellCooldown() end
+
+function GetSpellCooldownInt() end
+
+function GetStatusSourceID() end
+
+function GetStatusStackCount() end
+
+function GetStatusTimeRemaining() end
+
+function GetStep() end
+
+function GetTargetActionID() end
+
+function GetTargetFateID() end
+
+function GetTargetHP() end
+
+function GetTargetHPP() end
+
+function GetTargetHitboxRadius() end
+
+function GetTargetHuntRank() end
+
+function GetTargetMaxHP() end
+
+function GetTargetName() end
+
+function GetTargetObjectKind() end
+
+function GetTargetRawXPos() end
+
+function GetTargetRawYPos() end
+
+function GetTargetRawZPos() end
+
+function GetTargetRotation() end
+
+function GetTargetSubKind() end
+
+function GetTargetWorldId() end
+
+function GetTargetWorldName() end
+
+function GetToastNodeText() end
+
+function GetTradeableWhiteItemIDs() end
+
+function GetTrapLocations() end
+
+function GetType() end
+
+function GetWeeklyBingoOrderDataData() end
+
+function GetWeeklyBingoOrderDataKey() end
+
+function GetWeeklyBingoOrderDataText() end
+
+function GetWeeklyBingoOrderDataType() end
+
+function GetWeeklyBingoTaskStatus() end
+
+function GetZoneID() end
+
+function GetZoneInstance() end
+
+function GetZoneName() end
+
+function HasCondition() end
+
+function HasFlightUnlocked() end
+
+function HasMaxProgress() end
+
+function HasMaxQuality() end
+
+function HasPlugin() end
+
+function HasStats() end
+
+function HasStatus() end
+
+function HasStatusId() end
+
+function HasTarget() end
+
+function HasWeeklyBingoJournal() end
+
+function InSanctuary() end
+
+function InternalGetMacroText() end
+
+function IsAchievementComplete() end
+
+function IsAddonReady() end
+
+function IsAddonVisible() end
+
+function IsAetheryteUnlocked() end
+
+function IsCollectable() end
+
+function IsCrafting() end
+
+function IsFocusTargetCasting() end
+
+function IsFriendOnline() end
+
+function IsInFate() end
+
+function IsInZone() end
+
+function IsLeveAccepted() end
+
+function IsLevelSynced() end
+
+function IsLocalPlayerNull() end
+
+function IsMacroRunningOrQueued() end
+
+function IsMoving() end
+
+function IsNodeVisible() end
+
+function IsNotCrafting() end
+
+function IsObjectCasting() end
+
+function IsObjectInCombat() end
+
+function IsObjectMounted() end
+
+function IsPartyMemberCasting() end
+
+function IsPartyMemberInCombat() end
+
+function IsPartyMemberMounted() end
+
+function IsPauseLoopSet() end
+
+function IsPlayerAvailable() end
+
+function IsPlayerCasting() end
+
+function IsPlayerDead() end
+
+function IsPlayerOccupied() end
+
+function IsQuestAccepted() end
+
+function IsQuestComplete() end
+
+function IsStopLoopSet() end
+
+function IsTargetCasting() end
+
+function IsTargetInCombat() end
+
+function IsTargetMounted() end
+
+function IsVislandRouteRunning() end
+
+function IsWeeklyBingoExpired() end
+
+function LeaveDuty() end
+
+function LifestreamAbort() end
+
+function LifestreamAethernetTeleport() end
+
+function LifestreamExecuteCommand() end
+
+function LifestreamIsBusy() end
+
+function LifestreamTeleport() end
+
+function LifestreamTeleportToApartment() end
+
+function LifestreamTeleportToFC() end
+
+function LifestreamTeleportToHome() end
+
+function ListAllFunctions() end
+
+function LogDebug() end
+
+function LogInfo() end
+
+function LogVerbose() end
+
+function MoveItemToContainer() end
+
+function NavBuildProgress() end
+
+function NavIsAutoLoad() end
+
+function NavIsReady() end
+
+function NavPathfind() end
+
+function NavRebuild() end
+
+function NavReload() end
+
+function NavSetAutoLoad() end
+
+function NeedsRepair() end
+
+function ObjectHasStatus() end
+
+function OceanFishingIsSpectralActive() end
+
+function OpenRegularDuty() end
+
+function OpenRouletteDuty() end
+
+function PandoraGetFeatureConfigEnabled() end
+
+function PandoraGetFeatureEnabled() end
+
+function PandoraPauseFeature() end
+
+function PandoraSetFeatureConfigState() end
+
+function PandoraSetFeatureState() end
+
+function PartyMemberHasStatus() end
+
+function PathGetAlignCamera() end
+
+function PathGetMovementAllowed() end
+
+function PathGetTolerance() end
+
+function PathIsRunning() end
+
+function PathMoveTo() end
+
+function PathNumWaypoints() end
+
+function PathSetAlignCamera() end
+
+function PathSetMovementAllowed() end
+
+function PathSetTolerance() end
+
+function PathStop() end
+
+function PathfindAndMoveTo() end
+
+function PathfindInProgress() end
+
+function PauseYesAlready() end
+
+function QueryMeshNearestPointX() end
+
+function QueryMeshNearestPointY() end
+
+function QueryMeshNearestPointZ() end
+
+function QueryMeshPointOnFloorX() end
+
+function QueryMeshPointOnFloorY() end
+
+function QueryMeshPointOnFloorZ() end
+
+function QuestionableAddQuestPriority() end
+
+function QuestionableClearQuestPriority() end
+
+function QuestionableExportQuestPriority() end
+
+function QuestionableGetCurrentQuestId() end
+
+function QuestionableGetCurrentStepData() end
+
+function QuestionableImportQuestPriority() end
+
+function QuestionableInsertQuestPriority() end
+
+function QuestionableIsQuestLocked() end
+
+function QuestionableIsRunning() end
+
+function RSRAddBlacklistNameID() end
+
+function RSRAddPriorityNameID() end
+
+function RSRChangeOperatingMode() end
+
+function RSRRemoveBlacklistNameID() end
+
+function RSRRemovePriorityNameID() end
+
+function RSRTriggerSpecialState() end
+
+function RequestAchievementProgress() end
+
+function RestoreYesAlready() end
+
+function SelectDuty() end
+
+function SetAddersGCRank() end
+
+function SetAutoHookAutoGigSize() end
+
+function SetAutoHookAutoGigSpeed() end
+
+function SetAutoHookAutoGigState() end
+
+function SetAutoHookPreset() end
+
+function SetAutoHookState() end
+
+function SetClipboard() end
+
+function SetDFExplorerMode() end
+
+function SetDFJoinInProgress() end
+
+function SetDFLanguageD() end
+
+function SetDFLanguageE() end
+
+function SetDFLanguageF() end
+
+function SetDFLanguageJ() end
+
+function SetDFLevelSync() end
+
+function SetDFLimitedLeveling() end
+
+function SetDFMinILvl() end
+
+function SetDFSilenceEcho() end
+
+function SetDFUnrestricted() end
+
+function SetFlamesGCRank() end
+
+function SetMaelstromGCRank() end
+
+function SetMapFlag() end
+
+function SetNodeText() end
+
+function SetSNDProperty() end
+
+function TargetClosestEnemy() end
+
+function TargetClosestFateEnemy() end
+
+function TargetHasStatus() end
+
+function TeleportToGCTown() end
+
+function TerritorySupportsMounting() end
+
+function ToString() end
+
+function UseAutoHookAnonymousPreset() end
+
+function VislandIsRoutePaused() end
+
+function VislandSetRoutePaused() end
+
+function VislandStartRoute() end
+
+function VislandStopRoute() end
+
+function WeeklyBingoNumPlacedStickers() end
+
+function WeeklyBingoNumSecondChancePoints() end
+
+
 
 -- GetCharacterCondition()
 -- 
